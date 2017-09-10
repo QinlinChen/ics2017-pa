b0f2c726b87dd2599d427f5faa12799c863dac29
tracer-ics2017 <tracer@njuics.org>
2017-09-10 21:25:37 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-28-generic #32~16.04.2-Ubuntu SMP Thu Jul 20 10:19:48 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:25:37 up  8:17,  1 user,  load average: 0.19, 0.13, 0.05 a9f39ff51bc09a5df790fbdde39a42a77351872

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -46,7 +46,7 @@ static int cmd_si(char *args) {
 	if (!arg)
 		N = 1;				//if arg is NULL, execute 1 instruction
 	else if ((N = atoi(arg)) == 0)
-		fprintf(stderr, "Argument Error: Argument should be a number!");
+		fprintf(stderr, "Argument Error: Argument should be a number!\n");
 
 	int i;
 	for (i = 0; i < N; ++i)
@@ -57,7 +57,7 @@ static int cmd_si(char *args) {
 static int cmd_info(char *args) {
 	char *arg = strtok(NULL, " ");
 	if (!arg) {
-		fprintf(stderr, "Argument Error: There should be an argument!");
+		fprintf(stderr, "Argument Error: There should be an argument!\n");
 		return 0;
 	}
 	
@@ -73,7 +73,7 @@ static int cmd_info(char *args) {
 			printf("Something about watchpoint...\n");
 			break;
 		default:
-			fprintf(stderr, "Argument Error: Argument should be r or w!");
+			fprintf(stderr, "Argument Error: Argument should be r or w!\n");
 	}
 	return 0;
 }
@@ -81,7 +81,7 @@ static int cmd_info(char *args) {
 static int cmd_p(char *args) {
 	char *expr = strtok(NULL, " ");
 	if (!expr) {
-		fprintf(stderr, "Argument Error: There should be an argument!");
+		fprintf(stderr, "Argument Error: There should be an argument!\n");
 		return 0;
 	}
 	
@@ -93,7 +93,7 @@ static int cmd_p(char *args) {
 static int cmd_x(char *args) {
 	int N, expr;
 	if (sscanf(args, "%d %x", &N, &expr) != 2) {
-		fprintf(stderr, "Argument Error: Arguments should be N and EXPR");
+		fprintf(stderr, "Argument Error: Arguments should be N and EXPR\n");
 		return 0;
 	}			
 	
@@ -114,7 +114,7 @@ static int cmd_x(char *args) {
 static int cmd_w(char *args) {
 	char *expr = strtok(NULL, " ");
 	if (!expr) {
-		fprintf(stderr, "Argument Error: There should be an argument!");
+		fprintf(stderr, "Argument Error: There should be an argument!\n");
 		return 0;
 	}
 
@@ -126,7 +126,7 @@ static int cmd_w(char *args) {
 static int cmd_d(char *args) {
 	char *arg = strtok(NULL, " ");
 	if (!arg) {
-		fprintf(stderr, "Argument Error: There should be an argument!");
+		fprintf(stderr, "Argument Error: There should be an argument!\n");
 		return 0;
 	}
 	// int N = atoi(arg);
