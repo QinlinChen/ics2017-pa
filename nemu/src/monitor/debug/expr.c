cb5c23ca3f56a5691458a1cb603e211e0931692d
161220016-Chen Qinlin <742722969@qq.com>
2017-09-19 23:01:56 +0800

    change Log() to Log_write() in make_token()

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -122,7 +122,7 @@ static bool make_token(char *e) {
         int substr_len = pmatch.rm_eo;
 				
 				
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        Log_write("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
 
