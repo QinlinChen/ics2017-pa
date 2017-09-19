4362031f28569dfe50e6a732cfe31961d3a6c6c1
tracer-ics2017 <tracer@njuics.org>
2017-09-19 21:35:09 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:35:09 up  2:07,  1 user,  load average: 0.55, 0.22, 0.13 c155c314950280384533c0d9737d24586cdbddec

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -113,10 +113,12 @@ static bool make_token(char *e) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
-
+				
+				/*
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        position += substr_len;
+        */
+				position += substr_len;
 
         /* Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
