f3fad0b804bc541aafd2dc373e562f524e63bcb5
tracer-ics2017 <tracer@njuics.org>
2017-10-08 19:35:47 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:35:47 up  9:00,  1 user,  load average: 0.72, 0.55, 0.38 a9a342d7a6558d0ffa9f4447a777a3dfe4eaefc

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -42,11 +42,11 @@ make_EHelper(leave) {
 make_EHelper(cltd) {
   if (decoding.is_operand_size_16) {
     rtl_sext(&t0, &reg_l(R_EAX), 2);
-    //rtl_shri(&reg_l(R_EDX), &t0, 16);
-    rtl_mv(&reg_l(R_EDX), &tzero);
+    rtl_shri(&reg_l(R_EDX), &t0, 16);
   }
   else {
-    rtl_sari(&reg_l(R_EDX), &reg_l(R_EAX), 31);
+    //rtl_sari(&reg_l(R_EDX), &reg_l(R_EAX), 31);
+    rtl_mv(&reg_l(R_EDX), &tzero);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
