1c0f5b6f1bc4f024657285de86cf3331667b5e4d
tracer-ics2017 <tracer@njuics.org>
2017-10-08 20:11:26 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:11:26 up 3 min,  1 user,  load average: 0.34, 0.46, 0.21 5febf150a160860c308c1a6ffc63a4625a0f413d

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -107,7 +107,7 @@ opcode_entry opcode_table [512] = {
   /* 0x80 */	IDEXW(I2E, gp1, 1), IDEX(I2E, gp1), EMPTY, IDEX(SI2E, gp1),
   /* 0x84 */	IDEXW(G2E, test, 1), IDEX(G2E, test), IDEXW(G2E, xchg, 1), IDEX(G2E, xchg),
   /* 0x88 */	IDEXW(mov_G2E, mov, 1), IDEX(mov_G2E, mov), IDEXW(mov_E2G, mov, 1), IDEX(mov_E2G, mov),
-  /* 0x8c */	EMPTY, IDEX(lea_M2G, lea), EMPTY, EMPTY,
+  /* 0x8c */	EMPTY, IDEX(lea_M2G, lea), EMPTY, IDEX(E, pop),
   /* 0x90 */	IDEX(r2a, nop),  IDEX(r2a, xchg), IDEX(r2a, xchg), IDEX(r2a, xchg),
   /* 0x94 */	IDEX(r2a, xchg), IDEX(r2a, xchg), IDEX(r2a, xchg), IDEX(r2a, xchg),
   /* 0x98 */	EMPTY, EX(cltd), EMPTY, EMPTY,
