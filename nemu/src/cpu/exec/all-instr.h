a02cdae80893119060ec1ae6c3c7abf6d07784dd
tracer-ics2017 <tracer@njuics.org>
2017-10-08 18:27:07 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  18:27:07 up  7:55,  1 user,  load average: 0.29, 0.56, 0.53 519d922e2a900df3c1af778ea7e5ae3f46ac539

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -6,6 +6,7 @@ make_EHelper(movsx);
 make_EHelper(lea);
 make_EHelper(push);
 make_EHelper(pop);
+make_EHelper(leave);
 make_EHelper(xchg);
 
 make_EHelper(operand_size);
--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -33,8 +33,9 @@ make_EHelper(popa) {
 }
 
 make_EHelper(leave) {
-  TODO();
-
+  rtl_mv(&reg_l(R_ESP), &reg_l(R_EBP));
+  rtl_pop(&reg_l(R_EBP));
+  
   print_asm("leave");
 }
 
