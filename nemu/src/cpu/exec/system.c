66eba5e60f346303bd97e16b646c1dd8d0a086c7
tracer-ics2017 <tracer@njuics.org>
2017-10-13 22:40:13 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:40:13 up  2:50,  1 user,  load average: 0.59, 0.47, 0.45 61eaa8ca425ca0d7a7c12f19775f0339423ba1bc

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -47,6 +47,7 @@ void pio_write(ioaddr_t, int, uint32_t);
 make_EHelper(in) {
   t0 = pio_read(id_src->val, id_dest->width);
   operand_write(id_dest, &t0);
+  
   print_asm_template2(in);
 
 #ifdef DIFF_TEST
