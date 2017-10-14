f27d1f358aa22ceb6c7b4478bf9741110de7a32b
tracer-ics2017 <tracer@njuics.org>
2017-10-14 10:37:07 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:37:07 up  5:13,  1 user,  load average: 0.79, 0.38, 0.33 b15318fa79b72c512e08cf6638db24fbf6fa5b73

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -9,14 +9,24 @@
 
 uint8_t pmem[PMEM_SIZE];
 
+int is_mmio(paddr_t addr);
+uint32_t mmio_read(paddr_t addr, int len, int map_NO);
+void mmio_write(paddr_t addr, int len, uint32_t data, int map_NO);
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int map_NO;
+  if ((map_NO = is_mmio(addr)) < 0)
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  return mmio_read(addr, len, map_NO);
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int map_NO;
+  if ((map_NO = is_mmio(addr)) < 0)
+    memcpy(guest_to_host(addr), &data, len);
+  else
+    mmio_write(addr, len, data, map_NO);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
