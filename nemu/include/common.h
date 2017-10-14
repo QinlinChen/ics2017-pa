c998c6be7243f5d2c1cabedcdf6e1935a02f5fb0
tracer-ics2017 <tracer@njuics.org>
2017-10-14 17:48:05 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  17:48:05 up  1:08,  1 user,  load average: 0.34, 0.14, 0.09 f3ae4c423b47ba7bdae7e32c704d0e92b739b3e7

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -5,8 +5,7 @@
 #define DIFF_TEST
 
 /* You will define this macro in PA2 */
-//#define HAS_IOE
-volatile static int dummy;
+#define HAS_IOE
 
 #include "debug.h"
 #include "macro.h"
--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <assert.h>
 
-volatile static int dummy;
 #ifdef DEBUG
 extern FILE* log_fp;
 #	define Log_write(format, ...) \
