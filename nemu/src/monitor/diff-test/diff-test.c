e93386fd5633d623d9f7919981ba9219baba5cd1
tracer-ics2017 <tracer@njuics.org>
2017-10-14 21:16:51 +0800

    >  compile 161220016 ray Linux ray-VirtualBox 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:16:51 up  2:41,  1 user,  load average: 0.16, 0.17, 0.13 ce9562b6e7c4f7b818f6df71feec83c0c42900e3

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -149,7 +149,7 @@ void difftest_step(uint32_t eip) {
 
   // Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
-  if (r.eax == cpu.eax) {
+  if (r.eax != cpu.eax) {
     diff = true;
     Log("Detect difference at eip = 0x%x:\tcpu.eax = 0x%x\tr.eax = 0x%x)",
            eip, cpu.eax, r.eax);     
