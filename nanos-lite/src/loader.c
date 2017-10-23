9677adc3282e4d8e412bdb8fdc00df2984eba434
161220016-Chen Qinlin <742722969@qq.com>
2017-10-23 23:38:14 +0800

    pa3 first section completed(loader)

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,10 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+void ramdisk_read(void *buf, off_t offset, size_t len);
+size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
