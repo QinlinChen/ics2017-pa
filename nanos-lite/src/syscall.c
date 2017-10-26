f93ee22c958c11548862045b86e5eb39b0f46980
tracer-ics2017 <tracer@njuics.org>
2017-10-26 22:01:46 +0800

    >  run 161220016 ray Linux ray-VirtualBox 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:01:46 up  4:35,  1 user,  load average: 0.15, 0.11, 0.10 50e696f018ff64ec7c42078a5fd46b1bf5df0e2e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,17 @@
 #include "common.h"
 #include "syscall.h"
 
+_RegSet* sys_none(_RegSet *r) {
+  SYSCALL_ARG1(r) = 1;
+  return NULL;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
+    case SYS_none: return sys_none(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
