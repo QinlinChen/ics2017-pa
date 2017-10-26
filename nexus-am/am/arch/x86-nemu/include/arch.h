11e811cf92cb7d2274c2e35ec7ed6832a91ebe1f
tracer-ics2017 <tracer@njuics.org>
2017-10-26 21:35:13 +0800

    >  run 161220016 ray Linux ray-VirtualBox 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:35:13 up  4:08,  1 user,  load average: 0.59, 0.34, 0.18 a2a8df701ad38520354761322ed6d8b7d486060

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -7,9 +7,12 @@
 #define PGSIZE    4096    // Bytes mapped by a page
 
 struct _RegSet {
-  uintptr_t eflags, cs, eip, error_code;
-  int       irq;
-  uintptr_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  //uintptr_t eflags, cs, eip, error_code;
+  //int       irq;
+  //uintptr_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  int irq;
+  uintptr_t error_code, eip, cs, eflags;
 };
 
 #define SYSCALL_ARG1(r) 0
