a4e9625aec4f5ba75bd45c8fe221151b6f9901e2
tracer-ics2017 <tracer@njuics.org>
2017-10-27 13:13:23 +0800

    >  run 161220016 ray Linux ray-VirtualBox 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  13:13:23 up  6:56,  1 user,  load average: 0.39, 0.24, 0.17 babe0f7a8e53d873b6e360017b0150bcb36742b2

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -30,7 +30,7 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  return (void *)-1;
+  return (void *)_syscall_(SYS_brk, (uintptr_t)increment, 0, 0);
 }
 
 int _read(int fd, void *buf, size_t count) {
