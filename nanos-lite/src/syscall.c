307acccd357a9b197fa9978a09f552a2ef4f1701
tracer-ics2017 <tracer@njuics.org>
2017-10-27 12:44:17 +0800

    >  run 161220016 ray Linux ray-VirtualBox 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:44:17 up  6:27,  1 user,  load average: 0.62, 0.43, 0.28 c501b38b6a9d9150618944d2fbf51516c7126d08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,6 +11,19 @@ static inline _RegSet* sys_exit(_RegSet *r) {
   return NULL;
 }
 
+static inline _RegSet* sys_write(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG1(r);
+  const char *buf = (const char *)SYSCALL_ARG2(r);
+  size_t count = (size_t)SYSCALL_ARG3(r);
+  if (fd != 1 && fd != 2)
+    TODO();
+  int i;
+  for (i = 1; i <= count; ++i)
+    _putc(buf[i]);
+  SYSCALL_ARG1(r) = i;
+  return NULL;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -18,6 +31,7 @@ _RegSet* do_syscall(_RegSet *r) {
   switch (a[0]) {
     case SYS_none: return sys_none(r);
     case SYS_exit: return sys_exit(r);
+    case SYS_write: return sys_write(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
