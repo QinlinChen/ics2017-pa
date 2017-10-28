969f2430ff3acee873be6a985703950df35cb2e4
tracer-ics2017 <tracer@njuics.org>
2017-10-28 14:17:15 +0800

    >  run 161220016 ray Linux ray-VirtualBox 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:17:15 up 15:03,  1 user,  load average: 0.16, 0.17, 0.26 771abdf84ead514088fa94522df4073b3c66eccf

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,9 +21,11 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 //extern uint32_t* const fb;
 void fb_write(const void *buf, off_t offset, size_t len) {
   int x, y;
+  assert(offset % 4 == 0);
+  offset /= 4;
   y = offset / _screen.width;
   x = offset % _screen.width;  
-  _draw_rect((const uint32_t *)buf, x, y, len * sizeof(uint32_t), 1);
+  _draw_rect((const uint32_t *)buf, x, y, len / sizeof(uint32_t), 1);
   //memcpy((char *)fb + offset, buf, len);
 }
 
