a4cd45f214d3afdffa9d6ef886dc57ad28c807f0
tracer-ics2017 <tracer@njuics.org>
2017-12-09 19:58:46 +0800

    >  run 161220016 ray Linux ray-VirtualBox 4.10.0-40-generic #44~16.04.1-Ubuntu SMP Thu Nov 9 15:37:44 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:58:46 up 21:03,  1 user,  load average: 0.71, 0.63, 0.47 3e89a38a44628c1f2e40ab65b0f9b0e3fcf82be7

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,7 +7,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL: return do_syscall(r);
     case _EVENT_TRAP: return schedule(r);
-    case _EVENT_IRQ_TIME: printf("Hello from irq_time!\n"); return schedule(r);
+    case _EVENT_IRQ_TIME: return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
